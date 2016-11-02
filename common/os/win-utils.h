1c7564a97ed686a1ed0c62b92c2481bbae01a087
newenclave <pikrat.acid@gmail.com>
2016-11-02 16:57:42 +0300

    windows support

--- common/os/win-utils.h
+++ common/os/win-utils.h
@@ -7,20 +7,23 @@
 namespace utilities {
 
 struct charset {
-    static std::basic_string<char> make_mb_string( LPCWSTR src, UINT CodePage = CP_ACP )
+    static std::basic_string<char> make_mb_string( LPCWSTR src,
+                                                   UINT CodePage = CP_ACP )
     {
         int cch = WideCharToMultiByte( CodePage, 0, src, -1, 0, 0, 0, 0 );
         if( 0 != cch ) {
             std::vector<char> data;
             data.resize( cch + 1 );
 
-            WideCharToMultiByte( CodePage, 0, src, -1, &data[0], (DWORD)data.size( ), 0, 0 );
+            WideCharToMultiByte( CodePage, 0, src, -1, &data[0],
+                                 (DWORD)data.size( ), 0, 0 );
             return &data.front( );
         }
         return "";
     }
 
-    static std::basic_string<wchar_t> make_ws_string( LPCSTR src, UINT CodePage = CP_ACP )
+    static std::basic_string<wchar_t> make_ws_string( LPCSTR src,
+                                                      UINT CodePage = CP_ACP )
     {
 
         int cch = MultiByteToWideChar( CodePage, 0, src, -1, 0, 0 );
@@ -28,24 +31,25 @@ struct charset {
             std::vector<wchar_t> data;
 
             data.resize( cch + 1 );
-            MultiByteToWideChar( CodePage, 0, src, -1, &data[0], (DWORD)data.size( ));
+            MultiByteToWideChar( CodePage, 0, src, -1,
+                                 &data[0], (DWORD)data.size( ));
             return &data.front( );
         }
 
         return L"";
     }
 
-	static std::basic_string<char> make_utf8_string( const std::wstring &src )
-	{
-		return make_mb_string( src, CP_UTF8 );
-	};
-	
-	static std::basic_string<char> make_utf8_string( const std::string &src )
-	{
-		return make_mb_string( make_ws_string( src ), CP_UTF8 );
-	};
-	
-	static std::basic_string<char> make_utf8_string( LPCWSTR src )
+    static std::basic_string<char> make_utf8_string( const std::wstring &src )
+    {
+        return make_mb_string( src, CP_UTF8 );
+    };
+
+    static std::basic_string<char> make_utf8_string( const std::string &src )
+    {
+        return make_mb_string( make_ws_string( src ), CP_UTF8 );
+    };
+
+    static std::basic_string<char> make_utf8_string( LPCWSTR src )
     {
         return make_mb_string( src, CP_UTF8 );
     };
@@ -69,11 +73,13 @@ struct charset {
     {
         return src;
     }
-    static std::wstring make_ws_string( const std::string &src, UINT CodePage = CP_ACP )
+    static std::wstring make_ws_string( const std::string &src,
+                                        UINT CodePage = CP_ACP )
     {
         return make_ws_string( src.c_str( ), CodePage );
     }
-    static std::string make_mb_string( const std::wstring &src, UINT CodePage = CP_ACP )
+    static std::string make_mb_string( const std::wstring &src,
+                                       UINT CodePage = CP_ACP )
     {
         return make_mb_string( src.c_str( ), CodePage );
     }
