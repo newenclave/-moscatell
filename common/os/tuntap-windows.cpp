2d2b798c2a6e7555e53d3d7f8847f3b5019cdfb2
newenclave <pikrat.acid@gmail.com>
2016-11-02 16:54:42 +0300

    windows support

--- common/os/tuntap-windows.cpp
+++ common/os/tuntap-windows.cpp
@@ -15,17 +15,17 @@
 #include "boost/asio/ip/address.hpp"
 
 #define TAP_CONTROL_CODE(request, method) \
-        CTL_CODE(FILE_DEVICE_UNKNOWN, request, method, FILE_ANY_ACCESS)
+    CTL_CODE(FILE_DEVICE_UNKNOWN, request, method, FILE_ANY_ACCESS)
 #define TAP_IOCTL_CONFIG_TUN       TAP_CONTROL_CODE(10, METHOD_BUFFERED)
 #define TAP_IOCTL_SET_MEDIA_STATUS TAP_CONTROL_CODE( 6, METHOD_BUFFERED)
 
-#define	REGISTRY_CONTROL_PATH _T("SYSTEM\\CurrentControlSet\\Control")
+#define REGISTRY_CONTROL_PATH _T("SYSTEM\\CurrentControlSet\\Control")
 
 #define TAP_ADAPTER_KEY REGISTRY_CONTROL_PATH \
-                       _T("\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}")
+        _T("\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}")
 
 #define NETWORK_KEY REGISTRY_CONTROL_PATH \
-                       _T("\\Network\\{4D36E972-E325-11CE-BFC1-08002BE10318}")
+    _T("\\Network\\{4D36E972-E325-11CE-BFC1-08002BE10318}")
 
 #define TAP_DEVICE_SPACE     _T("\\\\.\\Global\\")
 #define TAP_VERSION_ID_0801  _T("tap0801")
@@ -35,317 +35,318 @@
 
 namespace msctl { namespace common {
 
-	using tstring = std::basic_string<TCHAR, 
-		            std::char_traits<TCHAR>, std::allocator<TCHAR> >;
+using tstring = std::basic_string<TCHAR,
+                    std::char_traits<TCHAR>, std::allocator<TCHAR> >;
 
-	namespace {
+namespace {
 
-		std::string get_error_string( DWORD	code, 
-									  HMODULE mod = NULL, 
-									  DWORD lang = 0 )
-		{
-			std::string tmp;
+    std::string get_error_string( DWORD	code,
+                                  HMODULE mod = NULL,
+                                  DWORD lang = 0 )
+    {
+        std::string tmp;
 
-			char *buf = NULL;
-			DWORD len = 0;
-			DWORD flags = FORMAT_MESSAGE_FROM_SYSTEM |
-				FORMAT_MESSAGE_ALLOCATE_BUFFER |
-				FORMAT_MESSAGE_IGNORE_INSERTS |
-				FORMAT_MESSAGE_MAX_WIDTH_MASK;
+        char *buf = NULL;
+        DWORD len = 0;
+        DWORD flags = FORMAT_MESSAGE_FROM_SYSTEM |
+                FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                FORMAT_MESSAGE_IGNORE_INSERTS |
+                FORMAT_MESSAGE_MAX_WIDTH_MASK;
 
-			if( mod ) {
-				flags |= FORMAT_MESSAGE_FROM_HMODULE;
-			}
+        if( mod ) {
+            flags |= FORMAT_MESSAGE_FROM_HMODULE;
+        }
 
-			len = FormatMessageA( flags, mod, code, lang,
-								  reinterpret_cast<char *>(&buf),
-								  0, NULL );
+        len = FormatMessageA( flags, mod, code, lang,
+                              reinterpret_cast<char *>(&buf),
+                              0, NULL );
 
-			if( (0 != len) && (NULL != buf) ) {
-				tmp.assign( buf, buf + len );
-				LocalFree( buf );
-			}
-			return tmp;
-		}
+        if( (0 != len) && (NULL != buf) ) {
+            tmp.assign( buf, buf + len );
+            LocalFree( buf );
+        }
+        return tmp;
+    }
 
-		void throw_runtime( const std::string &add )
-		{
-			throw std::runtime_error( std::string( add )
-									  + ". "
-									  + get_error_string( GetLastError( ) ) );
-		}
+    void throw_runtime( const std::string &add )
+    {
+        throw std::runtime_error( std::string( add )
+                                  + ". "
+                                  + get_error_string( GetLastError( ) ) );
+    }
 
-		struct win_reg_key {
+    struct win_reg_key {
 
-			HKEY key = NULL;
+        HKEY key = NULL;
 
-			win_reg_key( const win_reg_key & ) = delete;
-			win_reg_key &operator = ( const win_reg_key & ) = delete;
+        win_reg_key( const win_reg_key & ) = delete;
+        win_reg_key &operator = ( const win_reg_key & ) = delete;
 
-			win_reg_key( HKEY k )
-			{
-				assign( k );
-			}
+        win_reg_key( HKEY k )
+        {
+            assign( k );
+        }
 
-			win_reg_key( )
-			{ }
+        win_reg_key( )
+        { }
 
-			void assign( HKEY k )
-			{
-				RegCloseKey( key );
-				key = k;
-			}
+        void assign( HKEY k )
+        {
+            RegCloseKey( key );
+            key = k;
+        }
 
-			win_reg_key &operator = ( HKEY k )
-			{
-				assign( k );
-			}
+        win_reg_key &operator = ( HKEY k )
+        {
+            assign( k );
+        }
 
-			operator HKEY ( ) const
-			{
-				return key;
-			}
+        operator HKEY ( ) const
+        {
+            return key;
+        }
 
-			~win_reg_key( )
-			{
-				if( key != NULL ) {
-					RegCloseKey( key );
-				}
-			}
+        ~win_reg_key( )
+        {
+            if( key != NULL ) {
+                RegCloseKey( key );
+            }
+        }
 
-			HKEY *pget( )
-			{
-				return &key;
-			}
-		};
+        HKEY *pget( )
+        {
+            return &key;
+        }
+    };
 
-		struct bool_checker {
-			std::string call_name;
+    struct bool_checker {
+        std::string call_name;
 
-			bool_checker( ) { }
+        bool_checker( ) { }
 
-			bool_checker( const std::string &name )
-				:call_name( name )
-			{ }
+        bool_checker( const std::string &name )
+            :call_name( name )
+        { }
 
-			BOOL operator = ( BOOL s ) const
-			{
-				if( s == FALSE ) {
-					throw_runtime( call_name );
-				}
-				return s;
-			}
+        BOOL operator = ( BOOL s ) const
+        {
+            if( s == FALSE ) {
+                throw_runtime( call_name );
+            }
+            return s;
+        }
 
-			bool_checker operator ( )( const std::string &name ) const
-			{
-				return bool_checker( call_name + "." + name );
-			}
-		};
+        bool_checker operator ( )( const std::string &name ) const
+        {
+            return bool_checker( call_name + "." + name );
+        }
+    };
 
-		struct status_checker {
-			std::string call_name;
+    struct status_checker {
+        std::string call_name;
 
-			status_checker( ) { }
+        status_checker( ) { }
 
-			status_checker( const std::string &name )
-				:call_name( name )
-			{ }
+        status_checker( const std::string &name )
+            :call_name( name )
+        { }
 
-			LSTATUS operator = ( LSTATUS s ) const
-			{
-				if( s != ERROR_SUCCESS ) {
-					throw_runtime( call_name );
-				}
-				return s;
-			}
+        LSTATUS operator = ( LSTATUS s ) const
+        {
+            if( s != ERROR_SUCCESS ) {
+                throw_runtime( call_name );
+            }
+            return s;
+        }
 
-			status_checker operator ( )( const std::string &name ) const
-			{
-				return status_checker( call_name + "." + name );
-			}
-		};
+        status_checker operator ( )( const std::string &name ) const
+        {
+            return status_checker( call_name + "." + name );
+        }
+    };
 
-		tstring get_name( const tstring &dev_name )
-		{
-			static const tstring name = _T( "Name" );
-			status_checker chker( "get_name" );
-			DWORD dtype;
+    tstring get_name( const tstring &dev_name )
+    {
+        static const tstring name = _T( "Name" );
+        status_checker chker( "get_name" );
+        DWORD dtype;
 
-			tstring path = tstring( NETWORK_KEY ) + _T( "\\" )
-				+ dev_name + _T( "\\Connection" )
-				;
-			win_reg_key key;
-			chker( "RegOpenKeyEx" ) 
-				= RegOpenKeyEx( HKEY_LOCAL_MACHINE, path.c_str( ),
-								0, KEY_READ, key.pget( ) );
+        tstring path = tstring( NETWORK_KEY ) + _T( "\\" )
+                + dev_name + _T( "\\Connection" )
+                ;
+        win_reg_key key;
+        chker( "RegOpenKeyEx" )
+                = RegOpenKeyEx( HKEY_LOCAL_MACHINE, path.c_str( ),
+                                0, KEY_READ, key.pget( ) );
 
-			tstring res;
-			res.resize( 256 );
-			DWORD len = res.size( );
-			chker( "RegQueryValueEx" ) 
-				= RegQueryValueEx( key, name.c_str( ),
-								   NULL, &dtype,
-								   reinterpret_cast<LPBYTE>(&res[0]), &len );
-			res.resize( len );
-			return std::move( res );
-		}
-		
-		tstring get_device( const tstring &hint, tstring &outname )
-		{
-			status_checker chker( "get_device" );
-			win_reg_key key;
-			chker( "RegOpenKeyEx" ) = RegOpenKeyEx( HKEY_LOCAL_MACHINE,
-													TAP_ADAPTER_KEY, 0,
-													KEY_READ, key.pget( ) );
-			for( DWORD id = 0; ; id++ ) {
+        tstring res;
+        res.resize( 256 );
+        DWORD len = res.size( );
+        chker( "RegQueryValueEx" )
+                = RegQueryValueEx( key, name.c_str( ),
+                                   NULL, &dtype,
+                                   reinterpret_cast<LPBYTE>(&res[0]), &len );
+        res.resize( len );
+        return std::move( res );
+    }
 
-				TCHAR name[256];
-				DWORD len = 256;
-				DWORD dtype;
-				tstring comp;
+    tstring get_device( const tstring &hint, tstring &outname )
+    {
+        status_checker chker( "get_device" );
+        win_reg_key key;
+        chker( "RegOpenKeyEx" ) = RegOpenKeyEx( HKEY_LOCAL_MACHINE,
+                                                TAP_ADAPTER_KEY, 0,
+                                                KEY_READ, key.pget( ) );
+        for( DWORD id = 0; ; id++ ) {
 
-				auto status = RegEnumKeyEx( key, id, name,
-											&len, NULL, NULL, NULL, NULL );
-				if( status == ERROR_NO_MORE_ITEMS ) {
-					break;
-				}
+            TCHAR name[256];
+            DWORD len = 256;
+            DWORD dtype;
+            tstring comp;
 
-				chker( "RegEnumKeyEx" ) = status;
+            auto status = RegEnumKeyEx( key, id, name,
+                                        &len, NULL, NULL, NULL, NULL );
+            if( status == ERROR_NO_MORE_ITEMS ) {
+                break;
+            }
 
-				tstring unit = tstring( TAP_ADAPTER_KEY ) + _T( "\\" ) + name;
-				win_reg_key unitkey;
-				status = RegOpenKeyEx( HKEY_LOCAL_MACHINE, unit.c_str( ),
-									   0, KEY_READ, unitkey.pget( ) );
-				if( status != ERROR_SUCCESS ) {
-					continue;
-				}
+            chker( "RegEnumKeyEx" ) = status;
 
-				len = 256;
-				comp.resize( len );
-				status = RegQueryValueEx( unitkey, KEY_COMPONENT_ID, NULL, &dtype,
-										  reinterpret_cast<LPBYTE>(&comp[0]),
-										  &len );
-				if( status != ERROR_SUCCESS || dtype != REG_SZ ) {
-					continue;
-				}
-				comp.resize( len / sizeof( tstring::value_type ) - 1 );
-				if( comp == TAP_VERSION_ID_0801 || comp == TAP_VERSION_ID_0901 ) {
-					tstring device;
-					len = 1024;
-					device.resize( len );
-					status = RegQueryValueEx( unitkey, NET_CFG_INST_ID,
-											  NULL, &dtype,
-											  reinterpret_cast<LPBYTE>(&device[0]),
-											  (DWORD *)&len );
-					if( status != ERROR_SUCCESS || dtype != REG_SZ ) {
-						continue;
-					}
-					device.resize( len / sizeof( tstring::value_type ) - 1 );
-					auto hname = get_name( device );
+            tstring unit = tstring( TAP_ADAPTER_KEY ) + _T( "\\" ) + name;
+            win_reg_key unitkey;
+            status = RegOpenKeyEx( HKEY_LOCAL_MACHINE, unit.c_str( ),
+                                   0, KEY_READ, unitkey.pget( ) );
+            if( status != ERROR_SUCCESS ) {
+                continue;
+            }
 
-					if( !hint.empty( ) ) {
-						if( hname != hint ) {
-							continue;
-						}
-					}
-					outname.swap( hname );
-					return std::move( device );
-				}
-			}
-			return tstring( );
-		}
+            len = 256;
+            comp.resize( len );
+            status = RegQueryValueEx( unitkey, KEY_COMPONENT_ID, NULL, &dtype,
+                                      reinterpret_cast<LPBYTE>(&comp[0]),
+                    &len );
+            if( status != ERROR_SUCCESS || dtype != REG_SZ ) {
+                continue;
+            }
+            comp.resize( len / sizeof( tstring::value_type ) - 1 );
+            if( comp == TAP_VERSION_ID_0801 || comp == TAP_VERSION_ID_0901 ) {
+                tstring device;
+                len = 1024;
+                device.resize( len );
+                status = RegQueryValueEx( unitkey, NET_CFG_INST_ID,
+                                          NULL, &dtype,
+                                          reinterpret_cast<LPBYTE>(&device[0]),
+                        (DWORD *)&len );
+                if( status != ERROR_SUCCESS || dtype != REG_SZ ) {
+                    continue;
+                }
+                device.resize( len / sizeof( tstring::value_type ) - 1 );
+                auto hname = get_name( device );
 
-		HANDLE open_tun( const tstring &tun_device, tstring &name )
-		{
-			auto device = get_device( tun_device, name );
+                if( !hint.empty( ) ) {
+                    if( hname != hint ) {
+                        continue;
+                    }
+                }
+                outname.swap( hname );
+                return std::move( device );
+            }
+        }
+        return tstring( );
+    }
 
-			if( !device.empty( ) ) {
-				tstring tuntap = tstring( TAP_DEVICE_SPACE ) 
-					           + device + _T( ".tap" );
-				return CreateFile( tuntap.c_str( ), 
-						GENERIC_WRITE | GENERIC_READ, 0, 0,
-						OPEN_EXISTING,
-						FILE_ATTRIBUTE_SYSTEM | FILE_FLAG_OVERLAPPED,
-						NULL );
-			}
-			return INVALID_HANDLE_VALUE;
-		}
-	}
+    HANDLE open_tun( const tstring &tun_device, tstring &name )
+    {
+        auto device = get_device( tun_device, name );
 
-	src_dest_v4 extract_ip_v4( const char *data, size_t len )
-	{
-		auto hdr = reinterpret_cast<const IPV4_HDR *>(data);
-		if( extract_family( data, len ) != 4 ) { 
-			return src_dest_v4( );
-		}
-		return std::make_pair( hdr->saddr,
-							   hdr->daddr );
-	}
+        if( !device.empty( ) ) {
+            tstring tuntap = tstring( TAP_DEVICE_SPACE )
+                    + device + _T( ".tap" );
+            return CreateFile( tuntap.c_str( ),
+                               GENERIC_WRITE | GENERIC_READ, 0, 0,
+                               OPEN_EXISTING,
+                               FILE_ATTRIBUTE_SYSTEM | FILE_FLAG_OVERLAPPED,
+                               NULL );
+        }
+        return INVALID_HANDLE_VALUE;
+    }
 
-	int extract_family( const char *data, size_t len )
-	{
-		auto hdr = reinterpret_cast<const IPV4_HDR *>(data);
-		if( len < sizeof( *hdr ) ) {
-			return -1;
-		}
-		return hdr->version;
-	}
+} // namespace =======================================================
 
-	device_info open_tun( const std::string &hint_name )
-	{
-		device_info res;
-		tstring name;
-		res.handle = open_tun( hint_name, name );
-		if( res.handle == INVALID_HANDLE_VALUE ) {
-			throw_runtime( "open_tun" );
-		}
-		res.name = utilities::charset::make_utf8_string( name );
+    src_dest_v4 extract_ip_v4( const char *data, size_t len )
+    {
+        auto hdr = reinterpret_cast<const IPV4_HDR *>(data);
+        if( extract_family( data, len ) != 4 ) {
+            return src_dest_v4( );
+        }
+        return std::make_pair( hdr->saddr,
+                               hdr->daddr );
+    }
 
-		return std::move( res );
-	}
+    int extract_family( const char *data, size_t len )
+    {
+        auto hdr = reinterpret_cast<const IPV4_HDR *>(data);
+        if( len < sizeof( *hdr ) ) {
+            return -1;
+        }
+        return hdr->version;
+    }
 
-	void clone_handle( native_handle hdl )
-	{
-		CloseHandle( hdl );
-	}
+    device_info open_tun( const std::string &hint_name )
+    {
+        device_info res;
+        tstring name;
+        res.handle = open_tun( hint_name, name );
+        if( res.handle == INVALID_HANDLE_VALUE ) {
+            throw_runtime( "open_tun" );
+        }
+        res.name = utilities::charset::make_utf8_string( name );
 
-	void setup_device( native_handle dev,
-					   const std::string &ip,
-					   const std::string &otherip,
-					   const std::string &mask )
-	{
-		using av4 = boost::asio::ip::address_v4;
-		av4 sip = av4::from_string( ip );
-		av4 sother = av4::from_string( otherip );
-		av4 smask = av4::from_string( mask );
+        return std::move( res );
+    }
 
-		DWORD status = 1;
-		DWORD len;
-		DWORD ipdata[3] = { 
-			htonl( sip.to_ulong( ) ),
-			htonl( sip.to_ulong( ) & smask.to_ulong( ) ),
-			htonl( smask.to_ulong( ) )
-		};
+    void clone_handle( native_handle hdl )
+    {
+        CloseHandle( hdl );
+    }
 
-		bool_checker chkr( "tun_setip" );
+    void setup_device( native_handle dev,
+                       const std::string &ip,
+                       const std::string &otherip,
+                       const std::string &mask )
+    {
+        using av4 = boost::asio::ip::address_v4;
+        av4 sip = av4::from_string( ip );
+        av4 sother = av4::from_string( otherip );
+        av4 smask = av4::from_string( mask );
 
-		chkr( "DeviceIoControl(TAP_IOCTL_SET_MEDIA_STATUS)" )
-			= DeviceIoControl( dev, 
-							   TAP_IOCTL_SET_MEDIA_STATUS, &status,
-							   sizeof( status ), &status,
-							   sizeof( status ), &len, NULL );
+        DWORD status = 1;
+        DWORD len;
+        DWORD ipdata[3] = {
+            htonl( sip.to_ulong( ) ),
+            htonl( sip.to_ulong( ) & smask.to_ulong( ) ),
+            htonl( smask.to_ulong( ) )
+        };
 
-		chkr( "DeviceIoControl(TAP_IOCTL_CONFIG_TUN)" )
-			= DeviceIoControl( dev, 
-							   TAP_IOCTL_CONFIG_TUN, &ipdata,
-							   sizeof( ipdata ), &ipdata,
-							   sizeof( ipdata ), &len, NULL );
-	}
+        bool_checker chkr( "tun_setip" );
 
-	int del_tun( const std::string &name ) /// not supported
-	{
-		return 0;
-	}
+        chkr( "DeviceIoControl(TAP_IOCTL_SET_MEDIA_STATUS)" )
+                = DeviceIoControl( dev,
+                                   TAP_IOCTL_SET_MEDIA_STATUS, &status,
+                                   sizeof( status ), &status,
+                                   sizeof( status ), &len, NULL );
+
+        chkr( "DeviceIoControl(TAP_IOCTL_CONFIG_TUN)" )
+                = DeviceIoControl( dev,
+                                   TAP_IOCTL_CONFIG_TUN, &ipdata,
+                                   sizeof( ipdata ), &ipdata,
+                                   sizeof( ipdata ), &len, NULL );
+    }
+
+    int del_tun( const std::string &name ) /// not supported
+    {
+        return 0;
+    }
 }}
 
 
--- common/tuntap.h
+++ common/tuntap.h
@@ -16,43 +16,43 @@ namespace msctl { namespace common {
 
 #ifndef _WIN32
 
-	using stream_type = boost::asio::posix::stream_descriptor;
-	using tuntap_transport = async_transport::point_iface<stream_type>;
-	using native_handle = stream_type::native_handle_type;
-	static const native_handle TUN_HANDLE_INVALID_VALUE = -1;
+    using stream_type = boost::asio::posix::stream_descriptor;
+    using tuntap_transport = async_transport::point_iface<stream_type>;
+    using native_handle = stream_type::native_handle_type;
+    static const native_handle TUN_HANDLE_INVALID_VALUE = -1;
 
 #else
 
-	using stream_type = boost::asio::windows::stream_handle;
-	using tuntap_transport = async_transport::point_iface<stream_type>;
-	using native_handle = stream_type::native_handle_type;
-	static const native_handle TUN_HANDLE_INVALID_VALUE = INVALID_HANDLE_VALUE;
+    using stream_type = boost::asio::windows::stream_handle;
+    using tuntap_transport = async_transport::point_iface<stream_type>;
+    using native_handle = stream_type::native_handle_type;
+    static const native_handle TUN_HANDLE_INVALID_VALUE = INVALID_HANDLE_VALUE;
 
 #endif
 
     using addres_mask_v4 = std::pair<boost::asio::ip::address_v4,
-                                     boost::asio::ip::address_v4>;
+    boost::asio::ip::address_v4>;
     using addres_mask_v6 = std::pair<boost::asio::ip::address_v6,
-                                     boost::asio::ip::address_v6>;
+    boost::asio::ip::address_v6>;
 
     using src_dest_v4 = std::pair<std::uint32_t, std::uint32_t>;
 
-	src_dest_v4 extract_ip_v4( const char *data, size_t len );
-	int extract_family( const char *data, size_t len );
+    src_dest_v4 extract_ip_v4( const char *data, size_t len );
+    int extract_family( const char *data, size_t len );
 
 
-	struct device_info {
-		native_handle handle = INVALID_HANDLE_VALUE;
-		std::string	  name;
-	};
+    struct device_info {
+        native_handle handle = TUN_HANDLE_INVALID_VALUE;
+        std::string	  name;
+    };
 
-	device_info open_tun( const std::string &hint_name );
-	void clone_handle( native_handle hdl );
-	int del_tun( const std::string &name );
-	void setup_device( native_handle device,
-					   const std::string &ip,
-					   const std::string &otherip,
-					   const std::string &mask );
+    device_info open_tun( const std::string &hint_name );
+    void clone_handle( native_handle hdl );
+    int del_tun( const std::string &name );
+    void setup_device( native_handle device,
+                       const std::string &ip,
+                       const std::string &otherip,
+                       const std::string &mask );
 
 }}
 
