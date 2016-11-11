84b7ebfcac467fe1db87e8721e34849e4001774c
newenclave <pikrat.acid@gmail.com>
2016-11-11 16:32:32 +0300

    fix format

--- agent/main.cpp
+++ agent/main.cpp
@@ -219,14 +219,14 @@ int main( int argc, const char **argv )
             try {
                 throw;
             } catch( const std::exception &ex ) {
-                app.log( )( lvl::error, agent::thread_prefix::get( ) )
-                        << "[poll] Exception @" << std::hex
+                app.log( )( lvl::error, agent::thread_prefix::get( ), "poll" )
+                        << "[!] Exception @" << std::hex
                         << std::this_thread::get_id( )
                         << "; " << ex.what( )
                         ;
             } catch( ... ) {
-                app.log( )( lvl::error, agent::thread_prefix::get( ) )
-                        << "[poll] Exception @" << std::hex
+                app.log( )( lvl::error, agent::thread_prefix::get( ), "poll" )
+                        << "[!] Exception @" << std::hex
                         << std::this_thread::get_id( )
                         << "; ..."
                         ;
