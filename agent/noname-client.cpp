66f24ce1865eab614d6f65d3a34d6d479b731eb9
newenclave <pikrat.acid@gmail.com>
2017-01-27 12:28:32 +0300

    protocol

---
+++ agent/noname-client.cpp
@@ -0,0 +1,9 @@
+#include "srpc/client/connector/async/tcp.h"
+#include "srpc/client/connector/async/udp.h"
+
+#include "application.h"
+
+namespace msctl { namespace agent { namespace noname {
+
+}}}
+
---
+++ agent/noname-server.cpp
@@ -0,0 +1,10 @@
+#include "srpc/server/acceptor/async/tcp.h"
+#include "srpc/server/acceptor/async/udp.h"
+
+#include "application.h"
+
+namespace msctl { namespace agent { namespace noname {
+
+
+}}}
+
